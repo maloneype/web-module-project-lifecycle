ef20796bba44a727fea4d3df5256e313ecf2759d
Paul Maloney <paul@paulmaloney.com>
2024-06-21 07:33:20 -0700

    project submission

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,71 @@
-import React from 'react'
+import React from 'react';
+import { Component } from 'react';
+import axios from 'axios';
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+  constructor() {
+    console.log("constructor invoked")
+    super()
+    this.state = {
+      todos: [],
+      displayCompleted: true,
+      newTodo: ""
+    }
+  }
+
+  componentDidMount() {
+    console.log("CDM")
+    axios
+      .get("http://localhost:9000/api/todos")
+      .then(res => {
+        this.setState({todos: res.data.data})
+      })
+      .catch(err => console.log(err.message))
+  }
+
+  handleInputChange = event => {
+    this.setState({newTodo: event.target.value})
+    console.log(this.state.newTodo)
+  }
+
+  handleNewTodoSubmit() {
+    console.log(this.state.newTodo)
+    const newTodoObj = {
+      id: Date.now(),
+      name: this.state.newTodo,
+      completed: false
+    }
+    this.setState(prevState => ({
+      todos: prevState.todos.concat(newTodoObj),
+      newTodo: ""
+    }))
+  }
+
   render() {
-    return null
+    console.log("render invoked")
+    console.log(this.state)
+    return (
+    <div>
+      <ul>
+        {this.state.displayCompleted ? 
+          this.state.todos.map(td => {
+            return <li key={td.id}>{td.name} {td.completed ? "✔️ " : ""}</li>
+          }) :
+          this.state.todos.map(td => {
+            return !td.completed ? <li key={td.id}>{td.name}</li> : null
+          })
+        
+        }
+      </ul>
+      <form onSubmit={this.handleNewTodoSubmit}>
+        <input placeholder="type new todo here" onChange={this.handleInputChange}></input>
+        <button>submit</button>
+      </form>
+        
+
+    </div>
+    )
   }
 }
--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+import React from 'react'';
+import ReactDOM from 'react-dom'
+import App from './frontend/App'
+
 const server = require('./backend/server')
 
 const PORT = process.env.PORT || 9000
@@ -5,3 +9,5 @@ const PORT = process.env.PORT || 9000
 server.listen(PORT, () => {
   console.log(`listening on ${PORT}`)
 })
+
+
